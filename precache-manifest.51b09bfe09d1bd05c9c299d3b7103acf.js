f15bc1883fb674a323431b108b2462991ca61b8d
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-06 12:45:34 -0400

    Updates

---
+++ precache-manifest.51b09bfe09d1bd05c9c299d3b7103acf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d682d24064f9f7156a1c57681c99ce57",
+    "url": "/fem-easybank-landing/index.html"
+  },
+  {
+    "revision": "ee1f8f5fa73113ea7875",
+    "url": "/fem-easybank-landing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2c7b3e21611f45ad6d41",
+    "url": "/fem-easybank-landing/static/js/2.10903dd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fem-easybank-landing/static/js/2.10903dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1f8f5fa73113ea7875",
+    "url": "/fem-easybank-landing/static/js/main.ce81cc78.chunk.js"
+  },
+  {
+    "revision": "9edc2d88a02ce573a324",
+    "url": "/fem-easybank-landing/static/js/runtime-main.679a7059.js"
+  },
+  {
+    "revision": "db3d89069f8292defec0f180493a8817",
+    "url": "/fem-easybank-landing/static/media/icon-api.db3d8906.svg"
+  },
+  {
+    "revision": "d4cdc9d6b3c930ddaff8dc79acc005a1",
+    "url": "/fem-easybank-landing/static/media/icon-budgeting.d4cdc9d6.svg"
+  },
+  {
+    "revision": "1a81861b7aaa8c30169d65faf9027218",
+    "url": "/fem-easybank-landing/static/media/icon-facebook.1a81861b.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "/fem-easybank-landing/static/media/icon-instagram.e0814599.svg"
+  },
+  {
+    "revision": "2fc9c776f2bb1b98e1d23bc813261bd3",
+    "url": "/fem-easybank-landing/static/media/icon-onboarding.2fc9c776.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "/fem-easybank-landing/static/media/icon-pinterest.ef15af23.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "/fem-easybank-landing/static/media/icon-twitter.064c3171.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "/fem-easybank-landing/static/media/icon-youtube.8e2f566f.svg"
+  },
+  {
+    "revision": "1510e4a211f7a044236decc6d9b4afe9",
+    "url": "/fem-easybank-landing/static/media/image-confetti.1510e4a2.jpg"
+  },
+  {
+    "revision": "0f583e2b521e1590dd7ab8366c478227",
+    "url": "/fem-easybank-landing/static/media/image-currency.0f583e2b.jpg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/fem-easybank-landing/static/media/image-mockups.edc63e85.png"
+  },
+  {
+    "revision": "c619f6cb0554ff2c465d2bb6268cb493",
+    "url": "/fem-easybank-landing/static/media/image-plane.c619f6cb.jpg"
+  },
+  {
+    "revision": "b561f8d50067b0ce1c2d4e8b54671922",
+    "url": "/fem-easybank-landing/static/media/image-restaurant.b561f8d5.jpg"
+  },
+  {
+    "revision": "5661b7079a91dcc4b3c4046741994abe",
+    "url": "/fem-easybank-landing/static/media/logo-white.5661b707.svg"
+  },
+  {
+    "revision": "fe5c6dcee699e86a25a18112801ec30f",
+    "url": "/fem-easybank-landing/static/media/logo.fe5c6dce.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aebf6e57f824dd36febeff9b77d83a11.js"
+  "/fem-easybank-landing/precache-manifest.51b09bfe09d1bd05c9c299d3b7103acf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fem-easybank-landing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
